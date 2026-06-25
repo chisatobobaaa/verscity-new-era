0fd54cc35afd216569e75a3358bb6980574b5737
chisatobobaaa <chisatobobaaa@gmail.com>
2026-06-25 11:54:06 +0700

    Add files via upload

--- api/discord-me.js
+++ api/discord-me.js
@@ -1,7 +1,13 @@
-const { avatarUrl, readDiscordSession } = require("../lib/discord-auth");
+const { avatarUrl, clearDiscordCookies, readDiscordSession } = require("../lib/discord-auth");
 const { sendJson } = require("../lib/http-utils");
 
 module.exports = async function handler(request, response) {
+  if (request.method === "POST" || request.method === "DELETE") {
+    response.setHeader("Set-Cookie", clearDiscordCookies());
+    sendJson(response, 200, { ok: true, user: null });
+    return;
+  }
+
   const user = readDiscordSession(request);
   if (!user) {
     sendJson(response, 200, { ok: true, user: null });
